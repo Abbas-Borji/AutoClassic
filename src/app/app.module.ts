cc496ed31259147feecae46ee8aea96651f04894
Mohammad Semmo <mohammadsemmo81@gmail.com>
2022-11-15 13:00:25 +0200

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { CounterComponent } from './counter/counter.component';
+import { HomeComponent } from './home/home.component';
+import { BrandsComponent } from './brands/brands.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { ContactComponent } from './contact/contact.component';
+import { SloganComponent } from './slogan/slogan.component';
+import { ServicesComponent } from './services/services.component';
+import { FormSectionComponent } from './form-section/form-section.component';
+import { AddressComponent } from './address/address.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    CounterComponent,
+    HomeComponent,
+    BrandsComponent,
+    TestimonialsComponent,
+    AboutComponent,
+    FooterComponent,
+    OurServicesComponent,
+    ContactComponent,
+    SloganComponent,
+    ServicesComponent,
+    FormSectionComponent,
+    AddressComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
