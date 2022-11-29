089a766209e1098512e58ff6e41fc92b935a7726
Mohammad Semmo <mohammadsemmo81@gmail.com>
2022-11-29 13:02:03 +0200

    Footer icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  symbols = [
+    {
+      icon: faGoogle,
+      link: 'https://www.google.com/search?q=autoclassics+sweden+bro&oq=&aqs=chrome.2.69i59i450l4.1129311123j0j15&sourceid=chrome&ie=UTF-8#lrd=0x465fa59871f508eb:0x1e02e5c03c1b4722,1',
+      target: '_blank',
+    },
+    {
+      icon: faFacebook,
+      link: 'https://www.facebook.com/www.autoclassic.se',
+      target: '_blank',
+    },
+    { icon: faEnvelope, link: 'mailto:autoclassic@live.se' },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
