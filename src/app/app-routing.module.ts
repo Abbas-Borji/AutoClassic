74c8aa7fbfecf35561fa3f28ffed17d02e2d9d29
Mohammad Semmo <mohammadsemmo81@gmail.com>
2022-12-25 22:29:48 +0200

    Contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { OurServicesComponent } from './our-services/our-services.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'our-services', component: OurServicesComponent },
+  { path: 'contact', component: ContactComponent },
+]; // sets up routes constant where you define your routes
 
-
+// configures NgModule imports and exports
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
