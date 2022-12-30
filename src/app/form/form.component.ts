b747ca5679623fcb33a067421c210ed7617c6523
Mohammad Semmo <mohammadsemmo81@gmail.com>
2022-12-30 21:50:14 +0200

    Form fix

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -14,26 +14,29 @@ export class FormComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      'First Name': new FormControl('', Validators.required),
-      'Last Name': new FormControl('', Validators.required),
-      Email: new FormControl('', [Validators.required, Validators.email]),
-      Mobile: new FormControl('', Validators.required),
-      'Car Brand': new FormControl('', Validators.required),
-      'Car Model': new FormControl('', Validators.required),
-      'Serial Number': new FormControl('', Validators.required),
-      Meters: new FormControl('', Validators.required),
-      Service: new FormControl('', Validators.required),
-      'Year Made': new FormControl('', Validators.required),
-      'More Information': new FormControl(''),
+      'First Name': new FormControl(null, Validators.required),
+      'Last Name': new FormControl(null, Validators.required),
+      Email: new FormControl(null, [Validators.required, Validators.email]),
+      Mobile: new FormControl(null, Validators.required),
+      'Car Brand': new FormControl(null, Validators.required),
+      'Car Model': new FormControl(null, Validators.required),
+      'Serial Number': new FormControl(null, Validators.required),
+      Meters: new FormControl(null, Validators.required),
+      Service: new FormControl(null, Validators.required),
+      'Year Made': new FormControl(null, Validators.required),
+      'More Information': new FormControl(null),
     });
   }
 
-  onSubmit() {
+  onSubmit(formRef) {
     if (this.form.valid) {
       const formData = new FormData();
       for (const key in this.form.value) {
         formData.append(key, this.form.value[key]);
       }
+
+      this.form.reset();
+      formRef.resetForm();
 
       return this.http
         .post(
